d594dca0a5857cd21daed8a2a5267d1b4e945efe
Maggitarius <144776072+Maggitarius@users.noreply.github.com>
2023-10-01 22:15:24 +0300

    Add files via upload
    Первая практическая

--- Program.cs
+++ Program.cs
@@ -1,85 +1,115 @@
-﻿namespace Practice2
+﻿Console.WriteLine("Добро пожаловать в Калькулятор!");
+Console.WriteLine("Выберите операцию и введите её номер:");
+Console.WriteLine("1. Сложить 2 числа ");
+Console.WriteLine("2. Вычесть первое из второго");
+Console.WriteLine("3. Перемножить два числа");
+Console.WriteLine("4. Разделить первое на второе");
+Console.WriteLine("5. Найти квадратный корень из числа");
+Console.WriteLine("6. Найти 1 процент из числа");
+Console.WriteLine("7. Возвести в степень N первое число");
+Console.WriteLine("8. Найти факториал из числа");
+Console.WriteLine("9. Выйти из программы");
+int vibor = int.Parse(Console.ReadLine());
+double otvet = 0; 
+do
 {
-    internal class Program
-    {
-        static void Main(string[] args)
+        if (vibor >= 1 && vibor <= 9)
         {
-            while (true)
+            while (vibor != 9)
             {
-                Console.WriteLine("Добро пожаловать! Выберите программу, которой хотите воспользоваться:");
-                Console.WriteLine("1. Игра 'Угадай число'");
-                Console.WriteLine("2. Таблица умножения");
-                Console.WriteLine("3. Вывод делитей числа");
-                Console.WriteLine("Для выхода из программы, напишите 'Выход'");
-                string vbr = (Console.ReadLine());
-                if (vbr == "1")
+                if (vibor == 1)
                 {
-                    Ugadaika();
+                    Console.Write("Введите первое число:");
+                    double x = double.Parse(Console.ReadLine());
+                    Console.Write("Введите второе число:");
+                    double y = double.Parse(Console.ReadLine());
+                    otvet = x + y;
+                    Console.WriteLine("Результат:" + otvet);
+                    break;
                 }
-                if (vbr == "2")
+                if (vibor == 2)
                 {
-                    TabUmn();
+                    Console.Write("Введите первое число:");
+                    double x = double.Parse(Console.ReadLine());
+                    Console.Write("Введите второе число:");
+                    double y = double.Parse(Console.ReadLine());
+                    otvet = x - y;
+                    Console.WriteLine("Результат:" + otvet);
+                    break;
                 }
-                if (vbr == "3")
+                if (vibor == 3)
                 {
-                    DelChsl();
+                    Console.Write("Введите первое число:");
+                    double x = double.Parse(Console.ReadLine());
+                    Console.Write("Введите второе число:");
+                    double y = double.Parse(Console.ReadLine());
+                    otvet = x * y;
+                    Console.WriteLine("Результат:" + otvet);
+                    break;
                 }
-                if (vbr == "Выход")
+                if (vibor == 4)
+                {
+                    Console.Write("Введите первое число:");
+                    double x = double.Parse(Console.ReadLine());
+                    Console.Write("Введите второе число:");
+                    double y = double.Parse(Console.ReadLine());
+                    if (y != 0)
+                    {
+                        otvet = x / y;
+                        Console.WriteLine("Результат:" + otvet);
+                        break;
+                    }
+                    if (y == 0) ;
+                    {
+                        Console.WriteLine("Нельзя делить на ноль!");
+                    }
+
+                }
+                if (vibor == 5)
+                {
+                    Console.Write("Введите первое число:");
+                    double x = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Результат:" + Math.Sqrt(x));
+                    break;
+                }
+                if (vibor == 6)
+                {
+                    Console.Write("Введите первое число:");
+                    double x = double.Parse(Console.ReadLine());
+                    otvet = x / 100;
+                    Console.WriteLine("Результат:" + otvet);
+                    break;
+                }
+                if (vibor == 7)
+                {
+                    Console.Write("Введите первое число:");
+                    double x = double.Parse(Console.ReadLine());
+                    Console.Write("Введите степень:");
+                    double n = double.Parse(Console.ReadLine());
+                    otvet = Math.Pow(x, n);
+                    Console.WriteLine("Результат:" + otvet);
+                    break;
+                }
+                if (vibor == 8)
+                {
+                    Console.WriteLine("Введите число: ");
+                    int a = int.Parse(Console.ReadLine());
+                    int factorial = 1;
+                    for (int x = 1; x <= a; x++)
+                    {
+                        factorial *= x;
+                    }
+                    Console.WriteLine("Результат:" + factorial);
+                    break;
+                if  (vibor == 9)
                 {
                     break;
                 }
+                }
             }
         }
-        static void Ugadaika()
+        else
         {
-            Random rnd = new Random();
-            int num = rnd.Next(100);
-            while (num > 0)
-            {
-                Console.WriteLine("Введите число, которое было загадано:");
-                int mbnum = Convert.ToInt32(Console.ReadLine());
-                if (mbnum == num)
-                {
-                    Console.WriteLine("Поздравляю! Вы угадали число!");
-                    break;
-                }
-                if (mbnum > num)
-                {
-                    Console.WriteLine("Не угадали! Загаднное число меньше:(");
-                }
-                if (mbnum < num)
-                {
-                    Console.WriteLine("Не угадали! Загаднное число больше:(");
-                }
-            }
+            Console.WriteLine("ВВЕДИТЕ ЧИСЛО ОТ 1 ДО 9!");
         }
-        static void TabUmn()
-        {
-            int[,] tbl = new int[11, 11];
-            int x = tbl.GetLength(0);
-            int y = tbl.GetLength(1);      
-            for (int i = 1; i < x; ++i){
-                for (int j = 1; j < y; ++j) {
-                    tbl[i, j] = i * j;
-                    Console.Write(tbl[i, j] + "\t");
-                }
-                Console.WriteLine();
-            }
-        }
-        static void DelChsl()
-        {
-            {
-            Console.WriteLine("Введите число, делите которого хотите узнать:");
-            int n = int.Parse(Console.ReadLine());
-            for(int i = 1; i <= n; ++i)
-            {
-                if (n % i == 0)
-                { 
-                    Console.Write(i + "   ");
-                }
-            }
-            Console.WriteLine();
-            }
-        }
-    }
-}
+} while (vibor != 9);
